3c8fa47a2842444c2a1ddceef37225ccdab39c26
JM Lugo <lugoc.jose@outlook.com>
2024-04-25 13:17:07 -0700

    Export new components on barrel file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { PageNotFound } from "./ui/top-bar/not-found/PageNotFound";
+export { ProductsGrid } from "./products/products-grid/ProductsGrid";
+export { SideMenu } from "./ui/sidemenu/SideMenu";
 export { Title } from "./ui/title/Title";
 export { TopBar } from "./ui/top-bar/TopBar";
