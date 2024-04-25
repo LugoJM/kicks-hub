061949dc71ac7d2c7f189dab3f79836299b0ca31
JM Lugo <lugoc.jose@outlook.com>
2024-04-24 19:01:41 -0700

    Fixed fonts config file location

--- src/app/config/fonts.ts
+++
@@ -1,8 +0,0 @@
-import { Inter, Ubuntu } from "next/font/google";
-
-export const inter = Inter({ subsets: ["latin"] });
-
-export const titleFont = Ubuntu({
-    subsets : ["latin"],
-    weight : ['500', '700']
-});
---
+++ src/config/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Ubuntu } from 'next/font/google';
+
+
+export const inter = Inter({subsets : ["latin"]});
+
+export const titleFont = Ubuntu({
+    subsets : ["latin"],
+    weight : ["500", "700"]
+})
