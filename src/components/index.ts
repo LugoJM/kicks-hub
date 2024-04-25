c4728ea6724ad6bd4267eb39913a2b7e1a891784
JM Lugo <lugoc.jose@outlook.com>
2024-04-25 16:37:43 -0700

    Export new components on barrel file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,10 @@
+export { Footer } from "./ui/footer/Footer";
 export { PageNotFound } from "./ui/not-found/PageNotFound";
+export { ProductMobileSlideshow } from "./products/slideShow/ProductMobileSlideShow";
 export { ProductsGrid } from "./products/products-grid/ProductsGrid";
+export { ProductSlideshow } from "./products/slideShow/ProductSlideShow";
+export { QuantitySelector } from "./products/quantity-selector/QuantitySelector";
 export { SideMenu } from "./ui/sidemenu/SideMenu";
+export { SizeSelector } from "./products/size-selector/SizeSelector";
 export { Title } from "./ui/title/Title";
 export { TopBar } from "./ui/top-bar/TopBar";
