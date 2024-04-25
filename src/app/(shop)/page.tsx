612b7df7cbc17e52fab8955b15bec725d55e7c7c
JM Lugo <lugoc.jose@outlook.com>
2024-04-25 16:31:32 -0700

    Added ProductsGrid component to display products

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,11 +1,13 @@
-import Image from "next/image";
-import { titleFont } from "@/config/fonts";
-import { Title } from "@/components";
+import { ProductsGrid, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+
+const products = initialData.products;
 
 export default function Home() {
   return (
     <div>
       <Title title="Shop" subtitle="All products" className="mb-2" />
+      <ProductsGrid products={products}/>
     </div>
   );
 }
