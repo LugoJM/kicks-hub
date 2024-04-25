292f89f023c003e204a71840716e9706fe445e05
JM Lugo <lugoc.jose@outlook.com>
2024-04-25 16:38:36 -0700

    Added Link to access product & format currency

--- src/components/products/products-grid/ProductGridItem.tsx
+++ src/components/products/products-grid/ProductGridItem.tsx
@@ -11,25 +11,26 @@ interface Props {
 }
 
 export const ProductGridItem = ( { product } : Props) => {
-
-    const [displayImage, setDisplayImage] = useState(product.images[0]);
+  const [displayImage, setDisplayImage] = useState(product.images[0]);
   return (
     <div className="rounded-md overflow-hidden fade-in">
-        <Image 
+      <Link href={`/product/${product.slug}`}>
+      <Image
           src={`/products/${displayImage}`}
           alt={product.title}
           className="w-full object-cover rounded"
           width={500}
           height={500}
-          onMouseEnter={() => setDisplayImage(product.images[4])}
-          onMouseLeave={() => setDisplayImage(product.images[0])}
+          onMouseEnter={() => setDisplayImage( product.images[3] )  }
+          onMouseLeave={() => setDisplayImage( product.images[0] ) }
         />
+      </Link>
 
         <div className="p-4 flex flex-col">
             <Link href={`/product/${product.slug}`} className="hover:text-blue-600">
                 {product.title}
             </Link>
-            <span className="font-bold">${product.price}</span>
+            <span className="font-bold">${product.price.toFixed(2)}</span>
         </div>
     </div>
   )
