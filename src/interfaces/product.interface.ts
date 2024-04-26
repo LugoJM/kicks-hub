fc42f91d5944087fbafe3108622885414f21b243
JM Lugo <lugoc.jose@outlook.com>
2024-04-25 19:07:08 -0700

    Change ValidSizes type to strings

--- src/interfaces/product.interface.ts
+++ src/interfaces/product.interface.ts
@@ -14,5 +14,5 @@ export interface Product {
 }
 
 export type Category = 'men'|'women'|'kid'|'unisex';
-export type Size = 5|5.5|6|6.5|7|7.5|8|8.5|9|9.5|10|10.5|11|12|13;
+export type Size = "5"|"5.5"|"6"|"6.5"|"7"|"7.5"|"8"|"8.5"|"9"|"9.5"|"10"|"10.5"|"11"|"12"|"13";
 export type Type = 'sneakers'|'slides'|'shirts'|'jacket';
