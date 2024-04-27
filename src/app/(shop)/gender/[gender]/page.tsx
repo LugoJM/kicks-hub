8f11df0805e4a658d04dd2716f4340b1182e638a
JM Lugo <lugoc.jose@outlook.com>
2024-04-26 17:02:19 -0700

    Renamed route from category to gender

---
+++ src/app/(shop)/gender/[gender]/page.tsx
@@ -0,0 +1,41 @@
+export const revalidate = 60;
+
+import { getPaginatedProductWithImages } from "@/actions";
+import { Pagination, ProductsGrid, Title } from "@/components";
+import { Gender } from "@prisma/client";
+import { redirect } from "next/navigation";
+
+
+interface Props {
+  params : {
+    gender : string;
+  },
+  searchParams : {
+    page? : string;
+  }
+};
+
+
+export default async function CategoryPage( {params, searchParams} : Props) {
+  const page = searchParams.page ? +searchParams.page : 1;
+
+  const { gender } = params;
+
+  const { products, totalPages } = await getPaginatedProductWithImages({page, gender : gender as Gender});
+
+  if(products.length === 0) redirect(`/gender/${gender}`);
+
+  const label : Record<string, string> = {
+    men: "Mans",
+    women: "Womens",
+    kid: "Kids",
+    unisex : "Unisex"
+  };
+  return (
+    <>
+      <Title title={`${label[gender]} Products`} subtitle="All products" className="mb-2" />
+      <ProductsGrid products={products} />
+      <Pagination totalPages={totalPages} />
+    </>
+  );
+}
