e7d9b2e251a5617e8c96b1944d51e252357d76f7
JM Lugo <lugoc.jose@outlook.com>
2024-04-26 18:42:52 -0700

    Created server actions to get product & product stock by slug

---
+++ src/actions/products/get-product-slug.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+
+export const getProductBySlug = async (slug: string) => {
+  try {
+    const product = await prisma.product.findUnique({
+      include: {
+        ProductImage: {
+          select: {
+            url: true,
+          },
+        },
+      },
+      where: {
+        slug,
+      },
+    });
+
+    if (!product) return null;
+
+    return {
+      ...product,
+      images: product.ProductImage.map((image) => image.url),
+    };
+  } catch (error) {
+    throw new Error("Error while trying to fetch product by slug");
+  }
+};
