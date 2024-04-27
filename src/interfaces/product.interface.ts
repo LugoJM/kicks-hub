8a4699885da46c10f326aaf078fecef44b7ea819
JM Lugo <lugoc.jose@outlook.com>
2024-04-26 22:35:54 -0700

    Created CartProduct interface to use with cart-store for persistence

--- src/interfaces/product.interface.ts
+++ src/interfaces/product.interface.ts
@@ -13,6 +13,16 @@ export interface Product {
     gender: Category;
 }
 
+export interface CartProduct{
+    id : string;
+    slug : string;
+    title : string;
+    quantity : number;
+    size : string;
+    price : number;
+    image : string;
+}
+
 export type Category = 'men'|'women'|'kid'|'unisex';
 export type Size = "5"|"5.5"|"6"|"6.5"|"7"|"7.5"|"8"|"8.5"|"9"|"9.5"|"10"|"10.5"|"11"|"12"|"13";
 export type Type = 'sneakers'|'slides'|'shirts'|'jacket';
