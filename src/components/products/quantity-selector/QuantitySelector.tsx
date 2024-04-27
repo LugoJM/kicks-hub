516d09b365e35e3e8baa73290eb1ba877ca4a562
JM Lugo <lugoc.jose@outlook.com>
2024-04-26 22:34:28 -0700

    Modified QuantitySelector component to work with CSR

--- src/components/products/quantity-selector/QuantitySelector.tsx
+++ src/components/products/quantity-selector/QuantitySelector.tsx
@@ -1,30 +1,28 @@
 'use client';
 
-import { useState } from "react";
 import { IoAddCircleOutline, IoRemoveCircleOutline } from "react-icons/io5";
 
 interface Props {
     quantity : number;
+    onQuantityChange : (count : number) => void;
 }
 
-export const QuantitySelector = ( { quantity } : Props) => {
-  const [count, setCount] = useState(1);
+export const QuantitySelector = ( { quantity, onQuantityChange } : Props) => {
+  const onValueChange = (value : number) => {
+    if(quantity + value < 1) return;
 
-  const onQuantityChange = (value : number) => {
-    if(count + value < 1) return;
-
-    setCount(count + value);
+    onQuantityChange(quantity + value);
   };
 
   return (
     <div className="flex">
-        <button onClick={() => onQuantityChange(-1)}>
+        <button onClick={() => onValueChange(-1)}>
             <IoRemoveCircleOutline size={30}/>
         </button>
         
-        <span className="flex items-center justify-center w-20 mx-3 px-5 bg-gray-200 rounded-md">{count}</span>
+        <span className="flex items-center justify-center w-20 mx-3 px-5 bg-gray-200 rounded-md">{quantity}</span>
         
-        <button onClick={() => onQuantityChange(1)}>
+        <button onClick={() => onValueChange(1)}>
             <IoAddCircleOutline size={30}/>
         </button>
     </div>
