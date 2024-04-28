1f4e155949b5f737a94e8d37dfaded78d04bb17c
JM Lugo <lugoc.jose@outlook.com>
2024-04-27 21:58:41 -0700

    Added an optional onClick event

--- src/components/ui/sidemenu/SideMenuLink.tsx
+++ src/components/ui/sidemenu/SideMenuLink.tsx
@@ -4,12 +4,14 @@ interface Props {
     href : string;
     icon : React.ReactNode;
     title : string;
+    onClick? : () => void;
 }
 
-export const SideMenuLink = ({ href, icon, title }: Props) => {
+export const SideMenuLink = ({ href, icon, title, onClick }: Props) => {
   return (
     <Link
       href={href}
+      onClick={onClick}
       className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
     >
       {icon}
