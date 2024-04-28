529c917ae831013f0b5a9da04607b090d161c2c1
JM Lugo <lugoc.jose@outlook.com>
2024-04-27 21:51:21 -0700

    Created sketch for Profile Page

---
+++ src/app/(shop)/profile/page.tsx
@@ -0,0 +1,13 @@
+import { auth } from "@/auth.config";
+import { Title } from "@/components";
+
+export default async function ProfilePage() {
+  const session = await auth();
+  return (
+    <div>
+      <Title title="Profile" />
+      <pre>{JSON.stringify(session?.user, null, 2)}</pre>
+      <h3 className="text-3xl">{session?.user.role}</h3>
+    </div>
+  );
+}
