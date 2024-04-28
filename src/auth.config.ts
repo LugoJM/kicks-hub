f99ffffb0b0074db70fccf139004590eb2175d2f
JM Lugo <lugoc.jose@outlook.com>
2024-04-27 21:46:09 -0700

    NextAuth configurations & middlewares

---
+++ src/auth.config.ts
@@ -0,0 +1,67 @@
+import NextAuth, { type NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { z } from "zod";
+import prisma from "./lib/prisma";
+import bcrypt from 'bcryptjs';
+import { NextResponse } from "next/server";
+
+
+const requireAuthRoutes = [
+  "/checkout",
+  "/profile",
+  "/orders"
+];
+
+export const authConfig: NextAuthConfig = {
+  pages: {
+    signIn: "/auth/login",
+    newUser: "/auth/new-account",
+  },
+  callbacks : {
+    jwt({token,user}){
+      if(user){
+        token.data = user;
+      }
+      return token;
+    },
+    session({session, token}){
+      session.user = token.data as any;
+      return session;
+    },
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isAuthRequiredRoute = requireAuthRoutes.some(route => nextUrl.pathname.startsWith(route));
+      if (isAuthRequiredRoute) {
+        if (!isLoggedIn)return false;
+      } else if (isLoggedIn) {
+        return NextResponse.rewrite(nextUrl);
+      }
+      return true;
+    },
+  },
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(8) })
+          .safeParse(credentials);
+
+        if(!parsedCredentials.success) return null;
+        
+        const { email, password } = parsedCredentials.data;
+
+        const user = await prisma.user.findUnique({where : { email : email.toLowerCase() }});
+
+        if(!user) return null;
+
+        if(!bcrypt.compareSync(password, user.password)) return null;
+
+        const { password : _, ...userInfo } = user;
+
+        return userInfo;
+      },
+    }),
+  ],
+};
+
+export const { signIn, signOut, auth, handlers } = NextAuth(authConfig);
