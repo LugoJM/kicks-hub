0bd5da8de77875a87703af9e0812dc010188f4cd
JM Lugo <lugoc.jose@outlook.com>
2024-04-28 01:06:41 -0700

    Created country & address interfaces

---
+++ src/interfaces/address.interface.ts
@@ -0,0 +1,10 @@
+export interface Address {
+    name: string;
+    lastName: string;
+    address: string;
+    additionalAddress?: string;
+    postalCode: string;
+    city: string;
+    country: string;
+    phone: string;
+};
