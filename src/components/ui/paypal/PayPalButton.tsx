2f2a2f5cb3a21dd33e4bb879907b1ae552c0acd9
JM Lugo <lugoc.jose@outlook.com>
2024-04-29 01:09:20 -0700

    Created Paypal button client component

---
+++ src/components/ui/paypal/PayPalButton.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
+import {
+  CreateOrderData,
+  CreateOrderActions,
+  OnApproveData,
+  OnApproveActions,
+} from "@paypal/paypal-js";
+import { paypalCheckPayment, setTransactionId } from "@/actions";
+
+interface Props {
+  orderId: string;
+  amount: number;
+}
+
+export const PayPalButton = ({ orderId, amount }: Props) => {
+  const [{ isPending }] = usePayPalScriptReducer();
+
+  if (isPending) {
+    return (
+      <div className="animate-pulse space-y-4 mb-12">
+        <div className="h-11 bg-yellow-500 rounded" />
+        <div className="h-11 bg-gray-700 rounded" />
+      </div>
+    );
+  }
+
+  const roundedAmount = amount.toFixed(2).toString();
+
+  const createOrder = async (
+    data: CreateOrderData,
+    actions: CreateOrderActions
+  ) => {
+    const transactionId = await actions.order.create({
+      intent: "CAPTURE",
+      purchase_units: [
+        {
+         invoice_id : orderId,
+          amount: {
+            value: roundedAmount,
+            currency_code: "USD",
+          },
+        },
+      ],
+    });
+
+    const { ok } = await setTransactionId(orderId, transactionId);
+    if (!ok) {
+      throw new Error("Could not process payment.");
+    }
+
+    return transactionId;
+  };
+
+  const onApproved = async (
+    data: OnApproveData,
+    actions: OnApproveActions
+  ) => {
+    const details = await actions.order?.capture();
+    if(!details) return;
+
+    await paypalCheckPayment(details?.id ?? "");
+  };
+
+  return <PayPalButtons createOrder={createOrder} onApprove={onApproved} />;
+};
