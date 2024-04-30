82d2cd9fbf96861fb72b9c76c537ba2df7fc1148
JM Lugo <lugoc.jose@outlook.com>
2024-04-29 22:50:51 -0700

    Added export of new server actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,8 +1,13 @@
 export { authenticate } from "./auth/login";
+export { changeUserRole } from "./user/change-user-role";
+export { deleteProductImage } from "./products/delete-product-image";
 export { deleteUserAddress } from "./address/delete-user-address";
 export { getAllCountries } from "./country/get-all-countries";
+export { getCategories } from "./products/get-categories";
 export { getOrderDetails } from "./order/get-order-details-by-id";
+export { getPaginatedOrders } from "./order/get-paginated-orders";
 export { getPaginatedProductWithImages } from "./products/product-pagination";
+export { getPaginatedUsers } from "./user/get-paginated-users";
 export { getProductBySlug } from "./products/get-product-slug";
 export { getStockBySlug } from "./products/get-stock-slug";
 export { getStoredUserAddress } from "./address/get-user-address";
@@ -14,3 +19,4 @@ export { placeOrder } from "./order/place-order";
 export { registerUser } from "./auth/register";
 export { setTransactionId } from "./payments/set-transaction-id";
 export { setUserAddress } from "./address/set-user-address";
+export { updateProduct } from "./products/create-product-update";
