21fc1a348c7161f111472c4c906ac1decee3deda
JM Lugo <lugoc.jose@outlook.com>
2024-04-29 22:53:17 -0700

    Created ProductImage interface & delete unused types

--- src/interfaces/product.interface.ts
+++ src/interfaces/product.interface.ts
@@ -23,6 +23,9 @@ export interface CartProduct{
     image : string;
 }
 
-export type Category = 'men'|'women'|'kid'|'unisex';
-export type Size = "5"|"5.5"|"6"|"6.5"|"7"|"7.5"|"8"|"8.5"|"9"|"9.5"|"10"|"10.5"|"11"|"12"|"13";
-export type Type = 'sneakers'|'slides'|'shirts'|'jacket';
+export interface ProductImage {
+    id : number;
+    url : string;
+};
+
+type Category = 'men'|'women'|'kid'|'unisex';
