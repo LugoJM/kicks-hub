aab2a85d765b546b9cf52f102a2be795791c84ee
JM Lugo <lugoc.jose@outlook.com>
2024-05-13 20:08:00 -0700

    Modify profile page

--- src/app/(shop)/profile/page.tsx
+++ src/app/(shop)/profile/page.tsx
@@ -1,13 +1,31 @@
 import { auth } from "@/auth.config";
 import { Title } from "@/components";
+import { IoPersonOutline } from "react-icons/io5";
 
 export default async function ProfilePage() {
   const session = await auth();
+  const { id, name, role, email } = session!.user;
   return (
-    <div>
+    <>
       <Title title="Profile" />
-      <pre>{JSON.stringify(session?.user, null, 2)}</pre>
-      <h3 className="text-3xl">{session?.user.role}</h3>
-    </div>
+      <div className="flex flex-col p-2 items-center space-y-4">
+        <i className="shadow-lg rounded-full p-8 w-fit">
+          <IoPersonOutline size={180} />
+        </i>
+        <div className="flex text-2xl">
+          <span>Usuario: &nbsp;</span>
+          <h3 className="font-bold">{name}</h3>
+        </div>
+        <div className="flex text-2xl">
+          <span>Role: &nbsp;</span>
+          <h3 className="font-bold">{role}</h3>
+        </div>
+        <div className="flex text-2xl">
+          <span>Email: &nbsp;</span>
+          <h3 className="font-bold">{email}</h3>
+        </div>
+        <span className="text-sm text-black/55">UserID: {id}</span>
+      </div>
+    </>
   );
 }
